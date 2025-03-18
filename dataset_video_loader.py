9f995c6e205920773c33e5f971371c44ccd8a0da
Eliot Weber <165580105+eliotweber2@users.noreply.github.com>
2025-03-18 18:36:37 -0400

    Added video loading

---
+++ dataset_video_loader.py
@@ -0,0 +1,3 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
---
+++ video_loader.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+class StreamInterface:
+    def __init__(self,open,read_frame,error,close):
+        self.open_fn = open
+        self.read_frame_fn = read_frame
+        self.error_fn = error
+        self.close_fn = close
+
+    def open(self):
+        self.video_input = self.open_fn()
+
+    def read_frame(self):
+        return self.read_frame_fn(self.video_input)
+    
+    def error(self, error):
+        self.error_fn(error)
+    
+    def close(self):
+        self.close_fn(self.video_input)
+
+def read_and_process(stream_src, process_fn, stop_key='q'):
+    stream = stream_src()
+    stream.open()
+    while True:
+        frame = stream.read_frame()
+        process_fn(frame)
+
+        if stop_key and cv2.waitKey(1) & 0xFF == ord(stop_key):
+            break
+    stream.close()
