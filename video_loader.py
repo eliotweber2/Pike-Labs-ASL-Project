bf112f20955ee1d69cedf3a90601f80ab509aaee
Eliot Weber <165580105+eliotweber2@users.noreply.github.com>
2025-03-18 18:46:32 -0400

    Combined camera and video_loader

--- video_loader.py
+++ video_loader.py
@@ -1,6 +1,4 @@
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
 class StreamInterface:
     def __init__(self,open,read_frame,error,close):
@@ -21,6 +19,21 @@ class StreamInterface:
     def close(self):
         self.close_fn(self.video_input)
 
+def camera_stream_factory():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        return
+    
+    def read(cap):
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            return
+        return frame
+    
+    return StreamInterface(lambda: cv2.VideoCapture(0), lambda cap: read(cap), lambda x: print(x), lambda cap: cap.release)
+
 def read_and_process(stream_src, process_fn, stop_key='q'):
     stream = stream_src()
     stream.open()
@@ -31,3 +44,10 @@ def read_and_process(stream_src, process_fn, stop_key='q'):
         if stop_key and cv2.waitKey(1) & 0xFF == ord(stop_key):
             break
     stream.close()
+
+if __name__ == '__main__':
+    def process(frame):
+        cv2.imshow('frame', frame)
+        
+    read_and_process(camera_stream_factory, process)
+    cv2.destroyAllWindows()
